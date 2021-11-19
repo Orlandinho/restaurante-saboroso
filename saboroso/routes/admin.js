450e23e05b429dec93515fc7282ae81a457dbb51
Orlandinho <horlandomagic@gmail.com>
2021-11-18 22:17:48 -0300

    Login area administrativa

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./../inc/users');
 
 router.get('/', function(req, res, next) {
 
@@ -27,10 +28,31 @@ router.get('/emails', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
 
-    res.render('admin/login', {
+    users.render(req, res, null);
 
+});
 
-    });
+router.post('/login', function(req, res, next){
+
+    if(!req.body.email){
+
+        users.render(req, res, "Preencha o campo E-MAIL");
+    } else if (!req.body.password) {
+
+        users.render(req, res, "Preencha o campo SENHA");
+    } else {
+
+        users.login(req.body.email, req.body.password).then(user => {
+
+            req.session.user = user;
+
+            res.redirect("/admin");
+        }).catch(err => {
+
+            users.render(req, res, err.message || err);
+        })
+    }
+
 });
 
 router.get('/menus', function(req, res, next) {
